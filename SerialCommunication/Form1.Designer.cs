5404b608dd4950e3639d18e481e056b3492b30ee
NaanTeam <naanteam@gmail.com>
2013-11-13 21:25:23 -0800

    Initial Commit
    Very brief demo of how to interface with our sensor currently.
    Connor

---
+++ SerialCommunication/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace SerialCommunication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title4 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(174, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(68, 133);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "COM port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(174, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Turn on";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(299, 12);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series4.Color = System.Drawing.Color.Blue;
+            series4.IsVisibleInLegend = false;
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            series4.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(491, 353);
+            this.chart1.TabIndex = 6;
+            title4.Name = "Title1";
+            title4.Text = "X Acceleration";
+            this.chart1.Titles.Add(title4);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(4, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(58, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Refresh";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(68, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(83, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 398);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ SerialCommunication/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//REQUIRED
+using System.IO.Ports;
+
+
+namespace SerialCommunication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] ports = SerialPort.GetPortNames();
+
+            comboBox1.Items.Clear();
+            for (int i = 0; i < ports.Length; i++)
+            {
+                comboBox1.Items.Add(ports[i]);
+            }
+        }
+
+        SerialPort port1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text != "")
+            {
+                string portName = comboBox1.Text;
+                int badRate = 9600;
+                Parity par = Parity.None;
+                int dataBits = 8;
+                StopBits stpbits = StopBits.One;
+
+                port1 = new SerialPort(comboBox1.Text, badRate, par, dataBits, stpbits);
+                port1.Open();
+
+                port1.DataReceived += new SerialDataReceivedEventHandler(port1_DataReceived);
+            }
+            else
+            {
+                MessageBox.Show("Please select a Comm port. If none are listed, press refresh. If none appear" +
+                    " then no COM ports are open.");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //byte[] buffer = { (byte)'a', (byte)'b', (byte)'x', (byte)'e' };
+            //port1.Write(buffer, 0, buffer.Length);
+            timer1.Interval = 100;
+            timer1.Enabled = true;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+            comboBox1.Items.Clear();
+            for (int i = 0; i < ports.Length; i++)
+            {
+                comboBox1.Items.Add(ports[i]);
+            }
+
+        }
+
+
+        int n = 0;
+        private void port1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            double xAccel = double.Parse(port1.ReadLine());
+            textBox2.Text = xAccel.ToString();
+
+            n++;
+            chart1.Series[0].Points.AddXY(n, xAccel);
+
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //See SerialComm.h for magic number descriptions
+
+            //<StartTransmission> <Read Register> <Register Address> <End Transmission>
+            byte[] buffer = { 0x01, 0x02, 0x10, 0xFF };
+            port1.Write(buffer, 0, buffer.Length);
+        }
+
+
+        /*      #define SERIALCOMM_START_TOKEN      0x01
+                #define SERIALCOMM_READ_REGISTER    0x02
+                #define SERIALCOMM_WRITE_REGISTER   0x03
+                #define SERIALCOMM_END_TOKEN        0xFF
+
+
+                #define SERIALCOMM_REGISTER_XAcceleration       0x10
+                #define SERIALCOMM_REGISTER_YAcceleration       0x11
+                #define SERIALCOMM_REGISTER_ZAcceleration       0x12
+                #define SERIALCOMM_REGISTER_AcceloTemperature   0x13
+
+                #define SERIALCOMM_REGISTER_XAngularRate        0x20
+                #define SERIALCOMM_REGISTER_YAngularRate        0x21
+                #define SERIALCOMM_REGISTER_ZAngularRate        0x22
+                #define SERIALCOMM_REGISTER_GyroTemperature     0x23
+
+                #define SERIALCOMM_REGISTER_XMagneticVector     0x30
+                #define SERIALCOMM_REGISTER_YMagneticVector     0x31
+                #define SERIALCOMM_REGISTER_ZMagneticVector     0x32*/
+
+    }
+}
